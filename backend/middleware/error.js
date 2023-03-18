bf2e3a2ef793785cb122c6c93ffe5f220908ed19
Eric <eric2umeh@yahoo.com>
2023-03-18 19:46:57 +0100

    Update description in error.js

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,11 +2,11 @@ const ErrorHandler = require("../utils/ErrorHandler");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.message = err.message || "Internal server Error";
+  err.message = err.message || "Internal Server Error";
 
   // wrong mongodb id error
   if (err.name === "CastError") {
-    const message = `Resources not found with this id.. Invalid ${err.path}`;
+    const message = `Resources not found with this id... Invalid ${err.path}`;
     err = new ErrorHandler(message, 400);
   }
 
@@ -18,13 +18,13 @@ module.exports = (err, req, res, next) => {
 
   // wrong jwt error
   if (err.name === "JsonWebTokenError") {
-    const message = `Your url is invalid please try again letter`;
+    const message = `Your url is invalid, please try again later`;
     err = new ErrorHandler(message, 400);
   }
 
   // jwt expired
   if (err.name === "TokenExpiredError") {
-    const message = `Your Url is expired please try again letter!`;
+    const message = `Your token has expired, please try again later`;
     err = new ErrorHandler(message, 400);
   }
 
