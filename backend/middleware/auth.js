75dd5acceaeb70314875401fb2c1d8a63f19fe47
Eric <eric2umeh@yahoo.com>
2023-03-18 18:15:33 +0100

    Update scripts package.json

---
+++ backend/middleware/auth.js
@@ -0,0 +1,34 @@
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+const Shop = require("../model/shop");
+
+exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
+    const {token} = req.cookies;
+
+    if(!token){
+        return next(new ErrorHandler("Please login to continue", 401));
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+});
+
+
+exports.isSeller = catchAsyncErrors(async(req,res,next) => {
+    const {seller_token} = req.cookies;
+
+    if(!seller_token){
+        return next(new ErrorHandler("Please login to continue", 401));
+    }
+
+    const decoded = jwt.verify(seller_token, process.env.JWT_SECRET_KEY);
+
+    req.seller = await Shop.findById(decoded.id);
+
+    next();
+});
